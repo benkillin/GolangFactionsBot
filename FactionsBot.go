6ab0f511a5fa339821d00ce68ec6d9f12df6f5d3
ben <Github_benk@b1towel.com>
2019-06-23 05:45:50 +0000

    additional preliminary work - set up an automatically updating example config file and set up framework for dispatching the different commands

--- FactionsBot.go
+++ FactionsBot.go
@@ -7,10 +7,12 @@ import (
     "github.com/benkillin/ConfigHelper"
     log "github.com/sirupsen/logrus"
     "time"
+    "strings"
 )
 
 var (
     configFile = "factionsBotConfig.json"
+    defaultConfigFile = "factionsBotConfig.default.json" // this file gets overwritten every run with the current default config
     botID string // Bot ID
     config *Config
 )
@@ -20,7 +22,7 @@ type Config struct {
     Token string
     CommandPrefix string
     Logging LoggingConfig
-    Guilds []GuildConfig
+    Guilds map[string]GuildConfig
 }
 
 // LoggingConfig configuration as part of the config object.
@@ -33,18 +35,18 @@ type LoggingConfig struct {
 
 // GuildConfig represents the configuration of a single instance of this bot on a particular server/guild
 type GuildConfig struct {
-    GuildID string
+    GuildName string
     WallsEnabled bool
     WallsCheckTimeout time.Duration
     WallsCheckReminder time.Duration
     WallsCheckChannelID string
     WallsRoleMention string
-    Players []PlayerConfig
+    Players map[string]PlayerConfig
 }
 
 // PlayerConfig represents the players and their scores.
 type PlayerConfig struct {
-    PlayerID string
+    PlayerString string
     PlayerMention string
     WallChecks int
     LastWallCheck time.Time
@@ -59,9 +61,27 @@ func main() {
             Level: "trace",
             Format: "text",
             Output: "stderr",
-            Logfile: ""}} // the default config
+            Logfile: ""},
+            Guilds: map[string]GuildConfig{
+                "123456789012345678": GuildConfig{
+                    GuildName: "DerpGuild",
+                    WallsEnabled: false,
+                    WallsCheckTimeout: 15*time.Minute,
+                    WallsCheckReminder: 5*time.Minute,
+                    WallsCheckChannelID: "#123456789012345678",
+                    WallsRoleMention: "@&123456789012345678",
+                    Players: map[string]PlayerConfig{
+                        "123456789012345678": PlayerConfig{
+                            PlayerString: "Derp#1234",
+                            PlayerMention: "@123456789012345678",
+                            WallChecks: 0,
+                            LastWallCheck: time.Time{}}}}}} // the default config
     config = &Config{} // the running configuration
 
+    // This is debug code basically to keep the default json file updated which is checked into git.
+    os.Remove(defaultConfigFile)
+    ConfigHelper.GetConfigWithDefault(defaultConfigFile, defaultConfig, &Config{})
+    
     err := ConfigHelper.GetConfigWithDefault(configFile, defaultConfig, config)
 	if err != nil {
 		log.Fatalf("error loading/saving config/default config. %s", err)
@@ -83,11 +103,7 @@ func main() {
 
 	botID = bot.ID
 
-    d.AddHandler(testCmd)
-    d.AddHandler(setCmd)
-    d.AddHandler(helpCmd)
-    d.AddHandler(clearCmd)
-    d.AddHandler(weewooCmd)
+    d.AddHandler(messageHandler)
 
     err = d.Open()
 
@@ -101,70 +117,108 @@ func main() {
 }
 
 // our command handler function
-func testCmd(d *discordgo.Session, msg *discordgo.MessageCreate) {
+func messageHandler(d *discordgo.Session, msg *discordgo.MessageCreate) {
     user := msg.Author
     if user.ID == botID || user.Bot {
         return
     }
-    
-    log.Debugf("Incoming Message: %+v\n", msg.Message)
 
-    messageIds := make([]string, 0)
     content := msg.Content
 
-    if (content == config.CommandPrefix + "test") {
-        d.ChannelTyping(msg.ChannelID)
+    splitContent := strings.Split(content, " ")
 
-        err := d.ChannelMessageDelete(msg.ChannelID, msg.ID)
-        if err != nil {
-            log.Errorf("Error: Unable to delete incoming message: %s", err)
-            d.ChannelMessageSend(msg.ChannelID, fmt.Sprintf("Bot error: Unable to delete message: %s", err))
-        }
-
-        msgID := sendMsg(d, msg.ChannelID, fmt.Sprintf("Hello, %s, you have initated a test of the self destruct sequence!", msg.Author.Mention()))
-        messageIds = append(messageIds, msgID)
-
-        for i := 5; i > 0; i-- {
-            time.Sleep(1 * time.Second)
-            msgID := sendMsg(d, msg.ChannelID, fmt.Sprintf("%d", i))
-            messageIds = append(messageIds, msgID)
-        }
-
-        log.Printf("Message IDs to delete: %#v", messageIds)
-
-        err = d.ChannelMessagesBulkDelete(msg.ChannelID, messageIds)
-        if err != nil {
-            log.Errorf("Error: Unable to delete messages: %s", err)
-        }
+    switch splitContent[0]{
+    case config.CommandPrefix + "test":
+        testCmd(d, msg, msg.ChannelID)
+    case config.CommandPrefix + "set":
+        setCmd(d ,msg, msg.ChannelID)
+    case config.CommandPrefix + "clear":
+        clearCmd(d, msg, msg.ChannelID)
+    case config.CommandPrefix + "weewoo":
+        weewooCmd(d, msg, msg.ChannelID)
+    case config.CommandPrefix + "help":
+        helpCmd(d, msg, msg.ChannelID)
     }
 }
 
+func testCmd(d *discordgo.Session, msg *discordgo.MessageCreate, channelID string) {
+    log.Debugf("Incoming TEST Message: %+v\n", msg.Message)
+    messageIds := make([]string, 0)
+    log.Debugf("Mention of author: %s; String of author: %s; author ID: %s", msg.Author.Mention(), msg.Author.String(), msg.Author.ID)
+
+    deleteMsg(d, msg.ChannelID, msg.ID)
+    
+    msgID := sendMsg(d, msg.ChannelID, fmt.Sprintf("Hello, %s, you have initated a test of the self destruct sequence!", msg.Author.Mention()))
+    messageIds = append(messageIds, msgID)
+
+    for i := 5; i > 0; i-- {
+        msgID := sendMsg(d, msg.ChannelID, fmt.Sprintf("%d", i))
+        messageIds = append(messageIds, msgID)
+        time.Sleep(1500 * time.Millisecond) // it seems if it is 1 second or faster then discord itself will throttle.
+    }
+
+    time.Sleep(3 * time.Second)
+
+    err := d.ChannelMessagesBulkDelete(msg.ChannelID, messageIds)
+    if err != nil {
+        log.Errorf("Error: Unable to delete messages: %s", err)
+    }
+}
+
+func setCmd(d *discordgo.Session, msg *discordgo.MessageCreate, channelID string) {
+    sendTempMsg(d, channelID, "Settings command handler! TODO: this handler!", 5*time.Second)
+}
+
+func helpCmd(d *discordgo.Session, msg *discordgo.MessageCreate, channelID string) {
+    sendTempMsg(d, channelID, "Help command handler! TODO: this handler!", 5*time.Second)
+}
+
+func clearCmd(d *discordgo.Session, msg *discordgo.MessageCreate, channelID string) {
+    sendTempMsg(d, channelID, "Clear command handler! TODO: this handler!", 5*time.Second)
+}
+
+func weewooCmd(d *discordgo.Session, msg *discordgo.MessageCreate, channelID string) {
+    sendTempMsg(d, channelID, "Weewoo command handler! TODO: this handler!", 5*time.Second)
+}
+
+/*func Cmd(d *discordgo.Session, msg *discordgo.MessageCreate, channelID string) {
+}*/
+
+func hello() (string) {
+	return "Hello, world!"
+}
+
 func sendMsg(d *discordgo.Session, channelID string, msg string) (string) {
+    err := d.ChannelTyping(channelID)
+    if err != nil {
+        log.Errorf("Unable to send typing notification: %s", err)
+    }
+
     sentMessage, err := d.ChannelMessageSend(channelID, msg)
     if err != nil {
         log.Errorf("Unable to send message: %s", err)
         return ""
     }
+
     return sentMessage.ID
 }
 
-func setCmd(d *discordgo.Session, msg *discordgo.MessageCreate) {
+func deleteMsg(d *discordgo.Session, channelID string, messageID string) (error) {
+    err := d.ChannelMessageDelete(channelID, messageID)
+    if err != nil {
+        log.Errorf("Error: Unable to delete incoming message: %s", err)
+        return err
+    }
+
+    return nil
 }
 
-func helpCmd(d *discordgo.Session, msg *discordgo.MessageCreate) {
-}
-
-func clearCmd(d *discordgo.Session, msg *discordgo.MessageCreate) {
-}
-
-func weewooCmd(d *discordgo.Session, msg *discordgo.MessageCreate) {
-}
-
-/*func Cmd(d *discordgo.Session, msg *discordgo.MessageCreate) {
-}*/
-
-func hello() (string) {
-	return "Hello, world!"
+func sendTempMsg(d *discordgo.Session, channelID string, msg string, timeout time.Duration) {
+    go func() {
+        messageID := sendMsg(d, channelID, msg)
+        time.Sleep(timeout)
+        d.ChannelMessageDelete(channelID, messageID)
+    }()
 }
 
 func setupLogging(config *Config) {
